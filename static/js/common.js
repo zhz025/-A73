a7db6172e97d9a42411e6be721233892d17cc419
zhz025 <49409155+zhz025@users.noreply.github.com>
2020-03-13 00:19:25 -0700

    new plan for me

---
+++ static/js/common.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".page_a").click(function () {
+    $(".page_a").removeClass("active")
+    $(this).toggleClass('active')
+    // if(!this.isActive) {
+    // }
+    // this.isActive = !this.isActive
+  })
+})
